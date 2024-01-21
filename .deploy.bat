9b4dcc4be7108405f49ec0278f0f5cc059cc58e3
Rene <rene@karkkainen.net>
2024-01-21 18:01:04 +0200

    Updated deployment script

--- .deploy.bat
+++ .deploy.bat
@@ -1,20 +1,50 @@
 @echo off
+
+REM Run npm build
 call npm run build
 
-set destinationDirectory=C:\Users\ReneKarkkainen\GitHub\karkkainen.net
+REM Check if npm run build was successful
+if %errorlevel% neq 0 (
+    echo Failed to build the site.
+    exit /b %errorlevel%
+)
 
+REM Set destination directory
+set "destinationDirectory=C:\Users\ReneKarkkainen\GitHub\karkkainen.net"
+
+REM Copy files to destination directory
 xcopy /D /Y /E "C:\Users\ReneKarkkainen\GitHub\Website-Tool\dist\" "%destinationDirectory%"
+if %errorlevel% neq 0 (
+    echo Failed to copy static files to destination directory.
+    exit /b %errorlevel%
+)
+
 xcopy /D /Y /E "C:\Users\ReneKarkkainen\GitHub\Website-Tool\vanilla\" "%destinationDirectory%"
+if %errorlevel% neq 0 (
+    echo Failed to copy vanilla files to destination directory.
+    exit /b %errorlevel%
+)
 
-cd "%destinationDirectory%"
+REM Change to destination directory
+cd /d "%destinationDirectory%"
 
+REM Commit changes to Git
 git add -A
-git commit -m "Automatic Build Commit"
+git commit -m "Automatic Build Script"
 
+REM Push changes to the remote repository
 git push -u origin dev
+if %errorlevel% neq 0 (
+    echo Failed to push changes to the remote repository.
+    exit /b %errorlevel%
+)
 
-echo Site build, copied and pushed to dev remote server successfully!
+echo Site build, copied, and pushed to remote dev server successfully.
+echo Create a pull request to deploy to production server!
+
+REM Open GitHub compare URL
 start https://github.com/JAAKKQ/karkkainen.net/compare/main...dev?expand=1
 
+REM Wait for a few seconds
 timeout /t 5
-exit
+exit /b 0
